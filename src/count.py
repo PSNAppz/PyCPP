7627511363a16cc26e4bcac53569cbbcd07c8443
PSNAppZ <thepsnarayanan@gmail.com>
2022-09-13 19:47:56 +0530

    Init: Example code

---
+++ src/count.py
@@ -0,0 +1,25 @@
+import example
+import time
+
+# print 0 to 1 billion and calculate the time
+start = time.time()
+i = 0
+while(i<1000000000):
+   i = i + 1
+end = time.time()
+
+
+python_time = end - start
+
+# call example.print_range(1000000000) and calculate the time
+
+start = time.time()
+example.print_range(1000000000)
+end = time.time()
+
+cpp_time = end - start
+
+#print the time in seconds
+
+print("Python time: ", format(python_time, '.10f'), "s")
+print("C++ time: ", format(cpp_time, '.10f'), "s")
---
+++ src/example.cpp
@@ -0,0 +1,26 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/iostream.h>
+int add(int i, int j) {
+    return i + j;
+}
+
+// Create a function which prints 0 - 1billion
+void print_range(int n) {
+    size_t i = 0;
+    while (i < n) {
+        i++;
+    }
+}
+
+PYBIND11_MODULE(example, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("add", &add, "A function that adds two numbers");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+    m.def("print_range", &print_range, "Prints a range of numbers");
+}
